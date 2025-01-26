a0b79f7bbaf0722e243505be45762517f26c9141
Mosqueteiro <nat3.th3.gr3at@gmail.com>
2025-01-26 16:20:07 -0700

    update new_day script
    Added functionality for inserting new pixi commands into the
    pyproject.toml. Also fixed how the variables are interpretted and their
    formatting for various uses in the script.

--- scripts/new_day.sh
+++ scripts/new_day.sh
@@ -10,8 +10,8 @@ fi
 while [[ $# -gt 0 ]]; do
     case "$1" in
 	--day)
-		dayN="$2"
-		day="day$dayN"
+		dayN=$((10#$2))
+		day=$(printf "day%02d" $dayN)
 		shift 2
 		;;
 	--name)
@@ -27,9 +27,35 @@ done
 
 source .env
 
+echo "Creating directory: $day"
 mkdir "$day"
 touch "$day/$name"
 touch "$day/example.txt"
 curl "https://adventofcode.com/2024/day/$dayN/input" -H "Cookie: $COOKIE_SECRET" -o "$day/input.txt"
-# TODO: insert pixi tasks for day
+echo "Input received."
+
+PYPROJ="pyproject.toml"
+
+shopt -s lastpipe
+grep -no "^day[0-9]\{2\}" $PYPROJ | while IFS=: read line dayline; do
+	if (( $((10#${dayline#day})) > $dayN )); then
+		insert_line=$line
+		break
+	fi
+	prev_day=$dayline
+	prev_line=$line
+done
+
+if [ -z "$insert_line" ]; then
+	if [ -z "$prev_day" ]; then
+		TASK_HEAD=$(grep -n "tasks" $PYPROJ | cut -d: -f1)
+		insert_line=$(( $TASK_HEAD + 1 ))
+	else
+		insert_line=$(( $prev_line + 1 ))
+	fi
+fi
+
+sed -i "${insert_line}i ${day} = { cmd = \"ipython -i $name -- -f input.txt\", cwd = \"$day\" }" $PYPROJ
+sed -i "${insert_line}i ${day}-test = { cmd = \"ipython -i $name -- --test\", cwd = \"$day\" }" $PYPROJ
+echo "new pixi tasks for $day added"
 
