4a980d4aa07750c17cf00398205e9d184464ff48
Mosqueteiro <nat3.th3.gr3at@gmail.com>
2025-08-01 08:21:17 -0600

    add aoc func arg_parser_init
    initializes default arguments for Advent of Code challenges

--- src/adventofcode/aoc_utils.py
+++ src/adventofcode/aoc_utils.py
@@ -4,7 +4,32 @@ aoc_utils.py
 File for storing python utilities generic and useful across the days.
 """
 
+from argparse import ArgumentParser, FileType
 
+
+def arg_parser_init() -> ArgumentParser:
+    """Creates the default arguments for Advent of Code challenges
+
+    Arguments created are:
+    -f filename     file input to run code with, opens a file buffer with this file
+    -v              verbose output, use this as needed for debugging
+    --test          use this to indicate running a test file instead of input file
+
+    Returns
+    -------
+    ArgumentParser:
+        The parser with default arguments added to it. Run `parser.parse_args()`
+        to return a parsed dict
+    """
+    parser = ArgumentParser()
+    parser.add_argument("-f", type=FileType("r"), help="file input to run the code with")
+    parser.add_argument("-v", action="store_true", help="verbose")
+    parser.add_argument(
+        "--test",
+        action="store_true",
+        help="ignores file input and runs test file `example`",
+    )
+    return parser
 
 
 def print_part_divider(part: str) -> None:
